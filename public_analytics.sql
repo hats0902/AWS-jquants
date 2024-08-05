d87119a6848ea716a71918fab2b26306bfc2c387
Hatsuho Kamata <hatsuho.clover@gmail.com>
2024-08-05 16:27:16 +0900

    for public

--- public_analytics.sql
+++ public_analytics.sql
@@ -1,23 +1,23 @@
 -- 1. 特定の企業の2023年に開示された財務開示情報を取得する
 SELECT
-    *
+	*
 FROM
-    financial_disclosures AS fd
+	financial_disclosures AS fd
 JOIN
-    listed_companies AS lc
-    ON fd.LocalCode = lc.Code
+	listed_companies AS lc
+	ON fd.LocalCode = lc.Code
 WHERE
-    fd.DisclosedDate BETWEEN '2023-01-01' AND '2023-12-31'
-    AND lc.CompanyName = 'ラクス';
+	fd.DisclosedDate BETWEEN '2023-01-01' AND '2023-12-31'
+	AND lc.CompanyName = 'ラクス';
 
 
 -- 2. 各企業の最新の財務開示情報を取得する
 WITH rank_tbl AS(
 	SELECT
-  		*,
-  		RANK() OVER(PARTITION BY LocalCode ORDER BY DisclosedDate DESC, DisclosedTime DESC) rank
-  	FROM
-  		financial_disclosures
+		*,
+		RANK() OVER(PARTITION BY LocalCode ORDER BY DisclosedDate DESC, DisclosedTime DESC) rank
+	FROM
+		financial_disclosures
 		)
 SELECT
 	*
